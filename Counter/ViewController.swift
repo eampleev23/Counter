a978ab8b347801ad6262761f74741d22fbd038d2
ampleev.com <e@mpleev.com>
2025-08-23 03:43:37 +0300

    prj created

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Евгений Амплеев on 23.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
