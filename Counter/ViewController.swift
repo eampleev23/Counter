b0347b5a71f714c831f3703eb79efe60e272c77f
ampleev.com <e@mpleev.com>
2025-08-23 05:22:11 +0300

    final commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textViewLog: UITextView!
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var btnAddCount: UIButton!
+    @IBOutlet weak var btnSubCount: UIButton!
+    
+    @IBOutlet weak var counterValue: UILabel!
+    
+    public var count:Int = 0
+    public var log:String = "История изменений: \n"
+    
+    private func logEvent(event:String){
+    
+        var eventDescription:String = ""
+        
+        switch event{
+        case "add":
+            eventDescription = "значение изменено на +1"
+        case "sub":
+            eventDescription = "значение изменено на -1"
+        case "reset":
+            eventDescription = "значение сброшено"
+        case "subZero":
+            eventDescription = "попытка уменьшения значения счетчика ниже 0"
+        default:
+            eventDescription = "неизвестное событие"
+        
+        }
+        
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E, d MMM HH:mm:ss"
+        formatter.locale = Locale(identifier: "ru_RU")
+        log += "\(formatter.string(from: date)): \(eventDescription)\n"
+        textViewLog.text = log
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterValue.text = "\(count)"
+        textViewLog.isEditable = false
+        textViewLog.text = log
     }
 
-
+    @IBAction func btnAddCountTouchUpInsideEvent() {
+        count += 1
+        counterValue.text = "\(count)"
+        logEvent(event: "add")
+    }
+    
+    @IBAction func btnSubCountTouchInsideEvent() {
+        if count > 0 {
+            count -= 1
+            counterValue.text = "\(count)"
+            logEvent(event: "sub")
+        }else{
+            logEvent(event: "subZero")
+        }
+    }
+    @IBAction func btnResetCountTouchInsideEvent(_ sender: Any) {
+        if count != 0 {
+            count = 0
+            counterValue.text = "\(count)"
+            logEvent(event: "reset")
+        }
+    }
 }
 
