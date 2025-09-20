01283f9d87e7d312f641ce939ed5851eb84ab039
ampleev.com <e@mpleev.com>
2025-09-20 16:54:59 +0300

    add saving values log and count to user defaults

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,19 +9,32 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    
-    
+    // UITextView для отображения лога событий
     @IBOutlet weak private var textViewLog: UITextView!
+    
+    // Кнопка для сброса счетчика
     @IBOutlet weak private var btnReset: UIButton!
+    
+    // Кнопка для увеличения значения счетчика
     @IBOutlet weak private var btnAddCount: UIButton!
+    
+    // Кнопка для уменьшения значения счетчика
     @IBOutlet weak private var btnSubCount: UIButton!
     
+    // UILabel для отображения текущего значения счетчика
     @IBOutlet weak private var counterValue: UILabel!
     
     // Здесь необходимо брать значение из UserDefaults
-    
     public var count:Int = 0
     public var log:String = "История изменений: \n"
+    
+    private func loadCountFromUserDefaults(){
+        count = UserDefaults.standard.integer(forKey: "countValue")
+    }
+    
+    private func loadLogFromUserDefaults(){
+        log = UserDefaults.standard.string(forKey: "logValue") ?? ""
+    }
     
     private func logEvent(event:String){
     
@@ -46,11 +59,14 @@ final class ViewController: UIViewController {
         formatter.dateFormat = "E, d MMM HH:mm:ss"
         formatter.locale = Locale(identifier: "ru_RU")
         log += "\(formatter.string(from: date)): \(eventDescription)\n"
+        UserDefaults.standard.set(log, forKey: "logValue")
         textViewLog.text = log
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCountFromUserDefaults()
+        loadLogFromUserDefaults()
         counterValue.text = "\(count)"
         textViewLog.isEditable = false
         textViewLog.text = log
@@ -58,7 +74,7 @@ final class ViewController: UIViewController {
 
     @IBAction func btnAddCountTouchUpInsideEvent() {
         count += 1
-        UserDefaults.standard.set(count, forKey: "counterValue")
+        UserDefaults.standard.set(count, forKey: "countValue")
         counterValue.text = "\(count)"
         logEvent(event: "add")
     }
@@ -66,7 +82,7 @@ final class ViewController: UIViewController {
     @IBAction func btnSubCountTouchInsideEvent() {
         if count > 0 {
             count -= 1
-            UserDefaults.standard.set(count, forKey: "counterValue")
+            UserDefaults.standard.set(count, forKey: "countValue")
             counterValue.text = "\(count)"
             logEvent(event: "sub")
         }else{
@@ -76,7 +92,7 @@ final class ViewController: UIViewController {
     @IBAction func btnResetCountTouchInsideEvent(_ sender: Any) {
         if count != 0 {
             count = 0
-            UserDefaults.standard.set(count, forKey: "counterValue")
+            UserDefaults.standard.set(count, forKey: "countValue")
             counterValue.text = "\(count)"
             logEvent(event: "reset")
         }
